2ef80441e20c1ac13a00d05bd527213e4edfb24a
Anatoly Makarevich <anatoly_mak@yahoo.com>
2021-01-19 19:03:49 +0300

    Added calendar data.

--- m5/data/prep.py
+++ m5/data/prep.py
@@ -51,6 +51,54 @@ def get_ds_coords(sales: pd.DataFrame) -> xr.Dataset:
     return ds_coords
 
 
+def get_cal_indices(calendar: pd.DataFrame) -> xr.Dataset:
+    """Dataset of indicies for workday, month, year"""
+    cc = calendar.set_index("date")
+    c1 = cc[["wday", "month", "year"]].to_xarray()
+    return c1
+
+
+def get_cal_events(calendar: pd.DataFrame) -> xr.Dataset:
+    """Dataset of calendar events."""
+
+    cc = calendar.set_index("date")
+    s_names = pd.concat([cc["event_name_1"], cc["event_name_2"]])
+    s_types = pd.concat([cc["event_type_1"], cc["event_type_2"]])
+    e_names = pd.Index(s_names.dropna().unique(), name="event_name")
+
+    event_map = (
+        pd.DataFrame({"event_name": s_names, "event_type": s_types})
+        .dropna()
+        .drop_duplicates()
+        .set_index("event_name")["event_type"]
+    )
+
+    x = pd.DataFrame(0, index=cc.index, columns=e_names)
+    for enc in [c for c in cc.columns if "event_name_" in c]:
+        x += pd.get_dummies(cc[enc]).reindex(columns=e_names, fill_value=0)
+
+    c2 = xr.DataArray(x).to_dataset(name="event")
+    c2.coords["event_type"] = event_map
+    return c2
+
+
+def get_snap(calendar: pd.DataFrame, sales: pd.DataFrame) -> xr.Dataset:
+    """Gets dataframe of SNAP sales."""
+
+    cc = calendar.set_index("date")
+    cols_snap = {c: c.strip("snap_") for c in cc.columns if "snap_" in c}
+    df_snap = cc[list(cols_snap)].rename(columns=cols_snap)
+    df_snap.columns.name = "state_id"
+    w1 = df_snap.unstack().rename("snap").reset_index()
+    w2 = sales[["store_id", "state_id"]].drop_duplicates()
+    c3 = (
+        pd.merge(w1, w2, on=["state_id"])
+        .set_index(["date", "store_id"])[["snap"]]
+        .to_xarray()
+    )
+    return c3
+
+
 def get_ds_full(
     calendar: pd.DataFrame, sales: pd.DataFrame, sell_prices: pd.DataFrame
 ) -> xr.Dataset:
@@ -59,7 +107,10 @@ def get_ds_full(
     ds_sales = get_ds_sales(calendar, sales)
     ds_prices = get_ds_prices(calendar, sell_prices)
     ds_coords = get_ds_coords(sales)
-    ds = xr.merge([ds_coords, ds_sales, ds_prices])
+    c1 = get_cal_indices(calendar)
+    c2 = get_cal_events(calendar)
+    ds_snap = get_snap(calendar, sales)
+    ds = xr.merge([ds_coords, ds_sales, ds_prices, c1, c2, ds_snap])
 
     # Add time coord
     is_future = ds["sales"].isnull().all(dim=["item_id", "store_id"])
--- m5/data/ready.py
+++ m5/data/ready.py
@@ -1,17 +1,22 @@
 from m5 import __data__
-from .raw import calendar, sell_prices, sales_train_eval
-from .prep import get_ds_full
 
 import xarray as xr
 
 path_ds = __data__ / "ds.nc"
 
-__all__ = ["ds", "path_ds"]
+__all__ = ["load_ds", "path_ds"]
 
-if path_ds.exists():
-    ds = xr.load_dataset(path_ds)
-else:
-    ds = get_ds_full(
-        calendar=calendar, sales=sales_train_eval, sell_prices=sell_prices
-    )
-    ds.to_netcdf(path_ds)
+
+def load_ds(force_recalc: bool = False, cache: bool = True) -> xr.Dataset:
+    if force_recalc or path_ds.exists():
+        ds = xr.load_dataset(path_ds)
+    else:
+        from .raw import calendar, sell_prices, sales_train_eval
+        from .prep import get_ds_full
+
+        ds = get_ds_full(
+            calendar=calendar, sales=sales_train_eval, sell_prices=sell_prices
+        )
+        if cache:
+            ds.to_netcdf(path_ds)
+    return ds
